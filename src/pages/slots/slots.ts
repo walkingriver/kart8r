629e6a4996d5dd3f03a1a4644496c0b9c07f4a3b
Mike Callaghan <MikeCallaghan67@gmail.com>
2017-09-09 22:18:40 -0400

    Can randomize all now.

--- src/pages/slots/slots.ts
+++ src/pages/slots/slots.ts
@@ -13,12 +13,27 @@ export class SlotsPage {
   wheels: MkItem[];
   characters: MkItem[];
 
+  @ViewChild('char1') char1;
+  @ViewChild('char2') char2;
+  @ViewChild('char3') char3;
+  @ViewChild('char4') char4;
   @ViewChild('kart1') kart1;
   @ViewChild('kart2') kart2;
   @ViewChild('kart3') kart3;
   @ViewChild('kart4') kart4;
+  @ViewChild('wheel1') wheel1;
+  @ViewChild('wheel2') wheel2;
+  @ViewChild('wheel3') wheel3;
+  @ViewChild('wheel4') wheel4;
+  @ViewChild('wing1') wing1;
+  @ViewChild('wing2') wing2;
+  @ViewChild('wing3') wing3;
+  @ViewChild('wing4') wing4;
 
-  kartWheels = [];
+  charSpinner = [];
+  kartSpinner = [];
+  wheelSpinner = [];
+  wingSpinner = [];
 
   constructor(ms: MarioServiceProvider) {
     this.characters = ms.allCharacters();
@@ -30,15 +45,30 @@ export class SlotsPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad SlotsPage');
 
-    this.kartWheels[0] = this.kart1;
-    this.kartWheels[1] = this.kart2;
-    this.kartWheels[2] = this.kart3;
-    this.kartWheels[3] = this.kart4;
+    this.charSpinner[0] = this.char1;
+    this.charSpinner[1] = this.char2;
+    this.charSpinner[2] = this.char3;
+    this.charSpinner[3] = this.char4;
+    this.kartSpinner[0] = this.kart1;
+    this.kartSpinner[1] = this.kart2;
+    this.kartSpinner[2] = this.kart3;
+    this.kartSpinner[3] = this.kart4;
+    this.wheelSpinner[0] = this.wheel1;
+    this.wheelSpinner[1] = this.wheel2;
+    this.wheelSpinner[2] = this.wheel3;
+    this.wheelSpinner[3] = this.wheel4;
+    this.wingSpinner[0] = this.wing1;
+    this.wingSpinner[1] = this.wing2;
+    this.wingSpinner[2] = this.wing3;
+    this.wingSpinner[3] = this.wing4;
   }
 
   shuffle() {
-    for (let i = 0; i < this.kartWheels.length; i++) {
-      this.kartWheels[i].spin(2000);
+    for (let i = 0; i < 4; i++) {
+      this.charSpinner[i].spin(2000);
+      this.kartSpinner[i].spin(2000);
+      this.wheelSpinner[i].spin(2000);
+      this.wingSpinner[i].spin(2000);
     }
   }
 
