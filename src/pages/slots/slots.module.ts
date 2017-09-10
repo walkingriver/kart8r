a2667c0db11aa7991cf1bc448230fdbfce745923
Mike Callaghan <MikeCallaghan67@gmail.com>
2017-09-09 21:55:32 -0400

    Can randomize characters.

--- src/pages/slots/slots.module.ts
+++ src/pages/slots/slots.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SlotsPage } from './slots';
+import { ComponentsModule } from "../../components/components.module";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { SlotsPage } from './slots';
   ],
   imports: [
     IonicPageModule.forChild(SlotsPage),
+    ComponentsModule
   ],
 })
 export class SlotsPageModule {}
--- src/pages/slots/slots.ts
+++ src/pages/slots/slots.ts
@@ -1,12 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SlotsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+import { MkItem, MarioServiceProvider } from "../../providers/mario-service/mario-service";
 
 @IonicPage()
 @Component({
@@ -14,12 +8,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'slots.html',
 })
 export class SlotsPage {
+  karts: MkItem[];
+  wings: MkItem[];
+  wheels: MkItem[];
+  characters: MkItem[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('kart1') kart1;
+  @ViewChild('kart2') kart2;
+  @ViewChild('kart3') kart3;
+  @ViewChild('kart4') kart4;
+
+  kartWheels = [];
+
+  constructor(ms: MarioServiceProvider) {
+    this.characters = ms.allCharacters();
+    this.wheels = ms.allTires();
+    this.wings = ms.allWings();
+    this.karts = ms.allVehicles();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SlotsPage');
+
+    this.kartWheels[0] = this.kart1;
+    this.kartWheels[1] = this.kart2;
+    this.kartWheels[2] = this.kart3;
+    this.kartWheels[3] = this.kart4;
+  }
+
+  shuffle() {
+    for (let i = 0; i < this.kartWheels.length; i++) {
+      this.kartWheels[i].spin(2000);
+    }
   }
 
 }
